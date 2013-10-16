badbe26aa8ba71a90479261b4151a07671308a3c
Uwe <brecht.uwe@gmx.de>
2013-10-16 14:20:27 +0200

    Integrated webinterface

---
+++ DrinkController/inc/com.h
@@ -0,0 +1,34 @@
+#ifndef COM_H
+#define COM_H
+#include <string>
+#include <curl/curl.h>
+#include <vector>
+#define BUFFERSIZE 5000000
+
+struct ingredient_item{
+    std::string name;
+    int amount;
+};
+
+typedef struct {
+    int orderId;
+    std::vector<ingredient_item> ingredients;
+} Order_t;
+
+class Com
+{
+    private:
+        CURL *curl;
+        //char buffer[BUFFERSIZE];
+        std::string buffer;
+        CURLcode HtmlPutRequestOrder();
+        void JsonParseOrder(Order_t* order);
+        struct ingredient_item makeIngredientItem(std::string, int);
+    public:
+        void init();
+        void getOrder(Order_t* order);
+        void respondDone(); 
+        void cleanup();
+};
+
+#endif
