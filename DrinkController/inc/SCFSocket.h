31d31124b5423e7af1999b1389622736e3dd03b5
Uwe <brecht.uwe@gmx.de>
2013-10-16 08:55:02 +0200

    Fixed interface, added missing virtual

--- DrinkController/inc/SCFSocket.h
+++ DrinkController/inc/SCFSocket.h
@@ -1,7 +1,7 @@
 #ifndef _SCF_SOCKET_H_
 #define _SCF_SOCKET_H_
 
-#include <string.h>
+#include <string>
 
 typedef void (*ReceiveDataCallback_t)(char* pData, unsigned int numberOfData);
 
@@ -12,9 +12,10 @@ class SCFSocket
 
   /** Function opens the socket and listens on the port.
       @return The socketHandler.*/
-  int OpenSocket(std::string ip, int port, ReceiveDataCallback_t receiveCallback) = 0;
+  virtual int OpenSocket(std::string ip, int port, ReceiveDataCallback_t receiveCallback) = 0;
 
   /** Write data to socket **/
-  bool WriteData(int socketHandler, const char* pData, unsigned int numberOfData) = 0;
-}
+  virtual bool WriteData(int socketHandler, const char* pData, unsigned int numberOfData) = 0;
+};
+
 #endif
