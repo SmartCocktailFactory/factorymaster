3e93b58186135f0d2603fe2422c4612ae73b1106
Uwe <brecht.uwe@gmx.de>
2013-10-16 14:18:14 +0200

    Integrated webservice communication interface

--- DrinkController/src/DrinkController.cpp
+++ DrinkController/src/DrinkController.cpp
@@ -4,10 +4,13 @@
 #include "../inc/DrinkController.h"
 #include "../inc/BottleManagement.h"
 #include "../inc/LiquidDeliverySystem.h"
+#include "../inc/com.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /** Local function prototype **/
 static void* CommandManager(void *);
+static bool HaveToDeliverIceCubes(Order_t* pOrder);
 
 
 /** Local functions **/
@@ -28,27 +31,53 @@ static void* CommandManager(void *)
   LiquidDeliverySystemIndex_e stationIndex;
   std::string liquidName; 
   LiquidDeliverySystem sps( "127.0.0.1", 1999u );
-  bm.AssignBottleToLiquidStation( "Baccardi", E_LiquidDeliverySystemIndex_1 );
+  Com communicationInterface;
+  Order_t order;
+  
+  communicationInterface.init( );
+  bm.AssignBottleToLiquidStation( "Rum", E_LiquidDeliverySystemIndex_1 );
   bm.AssignBottleToLiquidStation( "Vodka", E_LiquidDeliverySystemIndex_2 );
   bm.AssignBottleToLiquidStation( "Cola", E_LiquidDeliverySystemIndex_3 );
   //  for (;;)
     {
       /* Poll order */
+      order.orderId = 0;
+      communicationInterface.getOrder( &order );
+      if (order.orderId != 0)
+	{
+	  fprintf( stderr, "New order:%d \n", order.orderId );
 
-      /* Ice necessary */
-
-
-      /* Look up bottles */
-      liquidName = "Baccardi";
-      stationIndex = bm.GetLiquidStationIndex( liquidName );
-      fprintf( stderr, "Index:%d \n", stationIndex );
-
-      /* Deliver liquid*/
-      sps.DeliverVolume( stationIndex, 10u );
-      /* Wait all done */
-
-      /* Push finishing */
-
+	  /* Ice necessary */
+	  if (HaveToDeliverIceCubes( &order ))
+	    {
+	      fprintf( stderr, "Order:%d with ice\n", order.orderId );
+	    }
+	  else
+	    {
+	      fprintf( stderr, "Order:%d no ice\n", order.orderId );
+	    }
+	  /* Deliver all liquids. */
+	  for (unsigned int i = 0u; i < order.ingredients.size( ); i++)
+	    {
+	      /* Look up bottles */
+	      liquidName = order.ingredients[i].name;
+	      stationIndex = bm.GetLiquidStationIndex( liquidName );
+	      fprintf( stderr, "Index:%d name:%s amount:%d\n", stationIndex, liquidName.c_str( ),
+		       order.ingredients[i].amount );
+	      
+	      /* Deliver liquid*/
+	      //sps.DeliverVolume( stationIndex, order.ingredients[i].amount );
+	      /* Wait all done */
+	      //sps.CheckDeliveryDoneSuccessfull( );
+	    }
+	  /* Push finishing */
+	  communicationInterface.respondDone( );
+	}
+      else
+	{
+	  sleep( 1u);
+	  fprintf( stderr, "No new order \n"  );
+	}
     }
 
     
@@ -58,4 +87,19 @@ static void* CommandManager(void *)
 int DrinkController::WaitForKill( )
 {
   return pthread_join( threadId, NULL ); 
+}
+
+static bool HaveToDeliverIceCubes(Order_t* pOrder)
+{
+  bool iceNecessary = false;
+  for (unsigned int i = 0u; i < pOrder->ingredients.size( ); i++)
+    {
+      if (pOrder->ingredients[i].name == "IceCube")
+	{
+	  iceNecessary = true;
+	  pOrder->ingredients.erase( pOrder->ingredients.begin() + i );
+	  break;
+	}
+    }
+  return iceNecessary;
 }
