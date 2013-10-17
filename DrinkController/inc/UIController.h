001d4ed3a3d0c3114d83d0b35d206c954a2fb0c4
Uwe <brecht.uwe@gmx.de>
2013-10-18 01:38:24 +0200

    Added missing files

---
+++ DrinkController/inc/UIController.h
@@ -0,0 +1,80 @@
+#ifndef _UI_CONTROLLER_H_
+#define _UI_CONTROLLER_H_
+
+/************************************************************
+    Includes
+*************************************************************/
+#include "../inc/LiquidDeliverySystem.h"
+#include "../inc/HueCom.h"
+
+/************************************************************
+    Defines
+*************************************************************/
+#define MAX_NUMBER_LED_LAMPS (3u)
+
+/************************************************************
+    Types
+*************************************************************/
+
+typedef enum
+    {
+        E_UIState_Unknown,
+        E_UIState_Starting,
+        E_UIState_Alarm,
+        E_UIState_Delivering,
+        E_UIState_DeliveringDone,
+        E_UIState_WaitingForOrder,
+        E_UIState_GlassMissing,
+    } UIGlobalState_e;
+
+
+    typedef enum
+    {
+        E_UIState_Bottle_Unknown,
+        E_UIState_Bottle_Empty,
+        E_UIState_Bottle_DeliveringDone,
+        E_UIState_Bottle_Delivering
+    } UIBottleState_e;
+    
+/************************************************************
+    Class definition
+*************************************************************/
+
+class UIController
+{
+ public:
+    static UIController* GetInstance()
+    {
+        if (instanz == NULL)
+        {
+            instanz = new UIController( );
+        }
+        return instanz;
+    }
+
+    void UpdateGlobalState(UIGlobalState_e newState);
+    void UpdateBottleState(LiquidDeliverySystemIndex_e index, UIBottleState_e newState);
+    
+ protected:
+    UIController( );
+        
+ private:
+    typedef enum
+    {
+        E_HueColor_Red = 0,
+        E_HueColor_Cyan = 32767,
+        E_HueColor_Yellow = 10922,
+        E_HueColor_Green = 29120,
+        E_HueColor_Blue = 43690,
+        E_HueColor_Orange = 5500
+    } HueColor_e;
+    
+    static UIController *instanz;
+    HueCom* pHue;
+    UIGlobalState_e globalState;
+    bool AllowedUpdateBottleState( );
+    void UpdateLamp(LiquidDeliverySystemIndex_e index, HueColor_e color, int saturation, int brightness);
+    void UpdateAllLamps(HueColor_e color, int saturation, int brightness);
+};
+
+#endif
---
+++ DrinkController/src/UIController.cpp
@@ -0,0 +1,101 @@
+/************************************************************
+    Includes
+*************************************************************/
+#include "../inc/UIController.h"
+#include "../inc/HueCom.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/************************************************************
+    Local types
+*************************************************************/
+
+/************************************************************
+     Local variables 
+*************************************************************/
+UIController* UIController::instanz = NULL;
+
+/************************************************************
+    Local function prototypes
+*************************************************************/
+
+/************************************************************
+    Public functions 
+*************************************************************/
+UIController::UIController( )
+{
+    globalState = E_UIState_Unknown;
+    pHue = new HueCom( );
+    pHue->init( );
+}
+
+void UIController::UpdateGlobalState(UIGlobalState_e newState)
+{
+    switch (newState)
+    {
+        case E_UIState_Unknown:
+            UpdateAllLamps( E_HueColor_Cyan, 255, 50 );
+            break;
+            
+        case E_UIState_Alarm:
+            UpdateAllLamps( E_HueColor_Red, 255, 50 );
+            break;
+            
+        case E_UIState_Delivering:
+            UpdateAllLamps( E_HueColor_Yellow, 255, 50 );
+            break;
+            
+        case E_UIState_DeliveringDone:
+            UpdateAllLamps( E_HueColor_Green, 255, 50 );
+            break;
+            
+        case E_UIState_WaitingForOrder:
+            UpdateAllLamps( E_HueColor_Green, 255, 50 );
+            break;
+            
+        case E_UIState_Starting:
+            UpdateAllLamps( E_HueColor_Blue, 255, 50 );
+            
+            break;
+        case E_UIState_GlassMissing:
+            UpdateAllLamps( E_HueColor_Orange, 255, 50 );
+            break;
+    }
+    globalState = newState;
+}
+
+void UIController::UpdateBottleState(LiquidDeliverySystemIndex_e index, UIBottleState_e newState)
+{
+    if (AllowedUpdateBottleState( ))
+    {
+        
+    }
+}
+    
+/************************************************************
+    Local functions
+*************************************************************/
+bool UIController::AllowedUpdateBottleState( )
+{
+    bool ret = false;
+    if (globalState == E_UIState_Delivering)
+    {
+        ret = true;
+    }
+    return ret;
+}
+
+void UIController::UpdateLamp(LiquidDeliverySystemIndex_e index, HueColor_e color,
+                              int saturation, int brightness)
+{
+    pHue->writeHUE( (int)index, (int) color, saturation, brightness );
+}
+   
+void UIController::UpdateAllLamps(HueColor_e color, int saturation, int brightness)
+{
+    for (int i = (int)E_LiquidDeliverySystemIndex_1;
+         i < (int)E_LiquidDeliverySystemIndex_Invalid; i++)
+    {
+        UpdateLamp( (LiquidDeliverySystemIndex_e)i, color, saturation, brightness );
+    } 
+}
